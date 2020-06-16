4fa9646846d1fddc1ac7014d24e3b6c579f52fe1
Lickitung <brandon_n_evans@live.com>
2020-06-16 16:42:36 -0500

    fix className attr for divs on homepage component

--- src/homepage.component.jsx
+++ src/homepage.component.jsx
@@ -4,32 +4,32 @@ import './homepage.styles.scss';
 
 const HomePage = () => (
     <div className='homepage'>
-        <div classname='directory-menu'>
-            <div classname='menu-item'>
+        <div className='directory-menu'>
+            <div className='menu-item'>
                 <div className='content'>
                     <h1 className='title'>HATS</h1>
                     <span className='subtitle'>SHOP NOW</span>
                 </div>
             </div>
-            <div classname='menu-item'>
+            <div className='menu-item'>
                 <div className='content'>
                     <h1 className='title'>JACKETS</h1>
                     <span className='subtitle'>SHOP NOW</span>
                 </div>
             </div>
-            <div classname='menu-item'>
+            <div className='menu-item'>
                 <div className='content'>
                     <h1 className='title'>SNEAKERS</h1>
                     <span className='subtitle'>SHOP NOW</span>
                 </div>
             </div>
-            <div classname='menu-item'>
+            <div className='menu-item'>
                 <div className='content'>
                     <h1 className='title'>WOMENS</h1>
                     <span className='subtitle'>SHOP NOW</span>
                 </div>
             </div>
-            <div classname='menu-item'>
+            <div className='menu-item'>
                 <div className='content'>
                     <h1 className='title'>MENS</h1>
                     <span className='subtitle'>SHOP NOW</span>
